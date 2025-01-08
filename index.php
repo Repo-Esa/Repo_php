a3874bb29876b68502f3be4298961488121357ba
Repo-Esa <esaazmifaiq902@gmail.com>
2025-01-08 08:56:53 +0700

    Task-oop

---
+++ index.php
@@ -0,0 +1,27 @@
+<?php
+require_once "Animal.php";
+require_once "Frog.php";
+require_once "Ape.php";
+
+echo "<pre>"; 
+$sheep = new Animal("shaun");
+echo "Name: " . $sheep->get_name() . "<br>";
+echo "Legs: " . $sheep->get_legs() . "<br>";
+echo "Cold Blooded: " . $sheep->get_cold_blooded() . "<br><br>";
+
+$kodok = new Frog("buduk");
+echo "Name: " . $kodok->get_name() . "<br>";
+echo "Legs: " . $kodok->get_legs() . "<br>";
+echo "Cold Blooded: " . $kodok->get_cold_blooded() . "<br>";
+echo "Jump: ";
+$kodok->jump();
+echo "<br><br>";
+
+$sungokong = new Ape("kera sakti");
+echo "Name: " . $sungokong->get_name() . "<br>";
+echo "Legs: " . $sungokong->get_legs() . "<br>";
+echo "Cold Blooded: " . $sungokong->get_cold_blooded() . "<br>";
+echo "Yell: ";
+$sungokong->yell();
+echo "</pre>";
+?>
